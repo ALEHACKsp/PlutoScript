1fc96177cb501ba7de91dfeb6bd1c66650415d57
Mark <chemnitz@arnsperger.org>
2018-02-17 20:57:03 +0100

    Added the hooking support. Added the support for multiple plugins hooking the same thing.

---
+++ PlutoScript/HookManager.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+std::vector<HookManager::OnSay> HookManager::Internal::OnSayCallbacks;
+HookManager::OnSay HookManager::Internal::OnSayReturn;
+
+namespace HookManager
+{
+	namespace Internal
+	{
+		void* DetourFunction(BYTE* src, BYTE* dst, const int len)
+		{
+			BYTE* jmp = (BYTE*)malloc(len + 5);
+			DWORD dwBack;
+
+			VirtualProtect(src, len, PAGE_EXECUTE_READWRITE, &dwBack);
+			memcpy(jmp, src, len);
+
+			jmp += len;
+			jmp[0] = 0xE9;
+
+			*(DWORD*)(jmp + 1) = (DWORD)(src + len - jmp) - 5;
+
+			src[0] = 0xE9;
+			*(DWORD*)(src + 1) = (DWORD)(dst - src) - 5;
+			for (int i = 5; i < len; i++)
+				src[i] = 0x90;
+			VirtualProtect(src, len, dwBack, &dwBack);
+
+			return (jmp - len);
+		}
+	}
+
+	void Internal::HookedOnSay(Entity* entity, int team, char* message)
+	{
+		for (auto &callback : OnSayCallbacks)
+			callback(entity, team, message);
+
+		return OnSayReturn(entity, team, message);
+	}
+
+	__declspec(dllexport) void InstallOnSay(OnSay onSay)
+	{
+		Internal::OnSayCallbacks.push_back(onSay);
+	}
+
+	void Initialize()
+	{
+		Internal::OnSayReturn = reinterpret_cast<OnSay>(Internal::DetourFunction(reinterpret_cast<BYTE*>(0x0047E900), reinterpret_cast<BYTE*>(Internal::HookedOnSay), 0x6));
+	}
+}
---
+++ PlutoScript/HookManager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Datatypes.h"
+#include <vector>
+
+namespace HookManager
+{	
+	using OnSay = void(_cdecl*)(Entity* entity, int team, char* message);
+
+	namespace Internal
+	{
+		void* DetourFunction(BYTE* src, BYTE* dst, const int len);
+		extern std::vector<OnSay> OnSayCallbacks;
+		extern OnSay OnSayReturn;
+		void HookedOnSay(Entity* entity, int team, char* message);
+	}
+
+	void Initialize();
+
+	__declspec(dllexport) void InstallOnSay(OnSay onSay);
+}
